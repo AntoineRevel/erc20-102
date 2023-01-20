8a17b2500fcc0a573ddb8445de11197d385286a2
Antoine Revel <antoine.revel@gmail.com>
2023-01-20 16:22:35 +0100

    ex1 (6)

--- contracts/AntoineRevel_SolutionTest.sol
+++ contracts/AntoineRevel_SolutionTest.sol
@@ -7,22 +7,37 @@ import "./ERC20Claimable.sol";
 contract AntoineRevel_SolutionTest {
     Evaluator evaluator;
     ERC20TD tdToken;
+    ERC20Claimable claimableToken;
 
     IExerciceSolution solution;
     
     constructor(IExerciceSolution _solution) public{
         evaluator=Evaluator(0x0F21621a753E072052F4E37eCc6d9c692e54F1C5);
         tdToken=ERC20TD(0x6084D43b9bE0234f5C67e3C768dAFf79dACB8e05);
+        claimableToken=ERC20Claimable(0x6dFD61555D8AB8BD542787F0A52d9D1e5dc1e6b4);
         solution=_solution;
+
         submitExercice();
     }
 
+    function startGetPoint() public{
+        ex1_claimedPoints();
+    }
+
+    function ex1_claimedPoints() private {
+        claimableToken.claimTokens();
+        evaluator.ex1_claimedPoints();
+    }
 
     function submitExercice() private {
         evaluator.submitExercice(solution);
     }
 
-    function getBalanceToken() public returns (uint256){
+    function getBalancePointToken() public view returns (uint256){
         return tdToken.balanceOf(address(this));
+    }
+
+    function getBalanceClaimableToken() public view returns (uint256){
+        return claimableToken.balanceOf(address(this));
     }
 }
--- migrations/2_solution_deploy.js
+++ migrations/2_solution_deploy.js
@@ -5,23 +5,27 @@ module.exports = (deployer, network, accounts) => {
     deployer.then(async () => {
         await deployExerciceSolution(deployer, network, accounts);
         await deployRecap(deployer, network, accounts);
-        await testSolution(deployer, network, accounts);
+        await printPoint();
+        await myTest.startGetPoint();
+        await printPoint();
     });
 };
-
-async function deployRecap(deployer, network, accounts) {
-    console.log("mySolution " + mySolution.address);
-    console.log("myTest " + myTest.address);
-
-}
 
 async function deployExerciceSolution(deployer, network, accounts) {
     mySolution= await ExerciceSolution.new();
     myTest= await ExerciceSolutionTest.new(mySolution.address);
 }
 
-async function testSolution(deployer, network, accounts) {
-    myBalance = await myTest.getBalanceToken.call();
-    console.log("My balance : " + myBalance)
+async function deployRecap(deployer, network, accounts) {
+    console.log("mySolution " + mySolution.address);
+    console.log("myTest " + myTest.address);
 }
+
+async function printPoint(){
+    myBalance = await myTest.getBalancePointToken.call();
+    console.log("My balance : " + myBalance);
+    myClaimableBalance = await myTest.getBalanceClaimableToken.call();
+    console.log("My ClaimableToken balance : " + myClaimableBalance);
+}
+
 
